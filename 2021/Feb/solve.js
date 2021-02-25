84ccc5958e320adf46a0d62192c000b2162419c5
Makan <makan.edrisi@gmail.com>
2021-02-26 06:01:48 +0800

    initializing

--- 2021/Feb/solve.js
+++ 2021/Feb/solve.js
@@ -119,19 +119,119 @@ const { mulMod, expMod, divMod, intDiv } = wasm.exports;
   //
   //
   //
-  const [readData] = await srcStream.readIntArray();
+  const [D, I, S, V, F] = await srcStream.readIntArray();
+
+  // streets
+  const intersecs = [];
+  for (let i = 0; i < I; ++i) {
+    intersecs.push([]);
+  }
+  const streets = [];
+  for (let i = 0; i < S; ++i) {
+    const [B, E, name, dur] = await srcStream.readArray();
+    const startIntersec = Number(B);
+    const endIntersec = Number(E);
+    const duration = Number(dur);
+    const inStIdx =
+      streets.push({
+        name,
+        duration,
+        startIntersec,
+        endIntersec,
+        used: false,
+      }) - 1;
+    intersecs[endIntersec].push({ inStIdx, queue: [], onTime: 0 });
+  }
+  // trips
+  const trips = [];
+  for (let i = 0; i < V; ++i) {
+    const [streetsNum, ...carTrips] = await srcStream.readArray();
+    const thisTrip = [];
+    let duration = 0;
+    carTrips.forEach((trp) => {
+      let idx;
+      streets.find((st, index) => {
+        if (st.name === trp) {
+          idx = index;
+          return true;
+        }
+      });
+      duration += streets[idx].duration;
+      thisTrip.push(idx);
+    });
+    if (duration <= D) {
+      trips.push({ streets: thisTrip, duration });
+    }
+  }
+  // remove unused trafic lights
+  for (let trip of trips) {
+    for (let stIdx of trip.streets) {
+      streets[stIdx].used = true;
+    }
+  }
+
+  for (let i = 0; i < I; ++i) {
+    intersecs[i] = intersecs[i].filter(
+      (tLight) => streets[tLight.inStIdx].used
+    );
+  }
+  // reorder
+  function reOrder() {
+    for (let i = 0; i < I; ++i) {
+      if (intersecs[i].length > 1) {
+        intersecs[i].unshift(intersecs[i].pop());
+      }
+    }
+  }
+  // initialize with random numbers
+  function randomize(span) {
+    for (let i = 0; i < I; ++i) {
+      const t = intersecs[i].length;
+      let remain = span;
+      for (let j = 0; j < t; ++j) {
+        if (j === t - 1) {
+          intersecs[i][j].onTime = remain;
+        } else {
+          const thisT = Math.round((remain * Math.random()) / t);
+          intersecs[i][j].onTime = thisT;
+          remain = remain - thisT;
+        }
+      }
+    }
+  }
 
   // task loop
   // q(Enter) for quit without save and any other keys(Enter) save the data
   function taskLoop() {
+    reOrder();
+    let span = Math.floor(Math.random() * D);
+    span = span ? span : 1;
+    randomize(span);
     //
     // Loop computation tasks
     //
   }
   // put save logic here
   function saveData() {
-    for (let i = 0; i < data.length; ++i) {
-      resultStream.arrayToFile([data[i].score]);
+    let numberOfI = 0;
+    for (let i = 0; i < I; ++i) {
+      if (intersecs[i].length) {
+        numberOfI++;
+      }
+    }
+    resultStream.arrayToFile([numberOfI]);
+    //
+    for (let i = 0; i < I; ++i) {
+      if (intersecs[i].length) {
+        resultStream.arrayToFile([i]);
+        resultStream.arrayToFile([intersecs[i].length]);
+        for (let tLight of intersecs[i]) {
+          resultStream.arrayToFile([
+            streets[tLight.inStIdx].name,
+            tLight.onTime,
+          ]);
+        }
+      }
     }
   }
   //
